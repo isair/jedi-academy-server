6a37e978f0ec51d5d0bfa51f815bd7e614e7d258
Baris Sencan <baris.sncn@gmail.com>
2018-03-10 11:51:07 +0000

    [yoda] Don't crash when sending a notification fails

--- server/yoda/managers/pushNotificationManager.py
+++ server/yoda/managers/pushNotificationManager.py
@@ -11,11 +11,14 @@ class PushNotificationManager:
     def send(message):
         assert isinstance(message, str)
         conn = httplib.HTTPSConnection("api.pushover.net:443")
-        # TODO: Get keys from a configuration file.
-        conn.request("POST", "/1/messages.json",
-                     urllib.urlencode({
-                         "token": "apegq7wck35j2zrijg6vv9furaj2qr",
-                         "user": "u693a8wszdyjvbd5s5td5qxe6gvrih",
-                         "message": message,
-                     }), {"Content-type": "application/x-www-form-urlencoded"})
-        conn.getresponse()
+        try:
+            # TODO: Get keys from a configuration file.
+            conn.request("POST", "/1/messages.json",
+                         urllib.urlencode({
+                             "token": "apegq7wck35j2zrijg6vv9furaj2qr",
+                             "user": "u693a8wszdyjvbd5s5td5qxe6gvrih",
+                             "message": message,
+                         }), {"Content-type": "application/x-www-form-urlencoded"})
+            conn.getresponse()
+        except Exception as e:
+            print(repr(e))
